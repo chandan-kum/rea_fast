9fabf6108ea0bd4229c1c2cd445e651d046646d8
chandankumar <singhchandan1184073+git@gmail.com>
2024-04-04 21:00:45 +0530

    first commit

---
+++ test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Custom.css';
+import Dashboard from './Components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+     <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
